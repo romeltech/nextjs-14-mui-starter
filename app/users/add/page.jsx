b2659436fcfa557314491496fe634d7a61b29537
Mel <indemnedev@gmail.com>
2024-04-14 23:07:47 +0400

    updated

--- app/users/add/page.jsx
+++ app/users/add/page.jsx
@@ -1,13 +1,14 @@
 import { TextField } from "@mui/material";
 // import { addUser } from "@/actions/UserActions";
+import { Paper, Grid, Box, Fab } from "@mui/material";
 
 const AddUserPage = ({}) => {
   return (
-    <div>
+    <Grid sx={{ width: "100%" }}>
       <form>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </form>
-    </div>
+    </Grid>
   );
 };
 
